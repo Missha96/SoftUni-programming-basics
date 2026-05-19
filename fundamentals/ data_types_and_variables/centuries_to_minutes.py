3b38fd3476d8ce20c7981015b9dc128d700eba49
Mihaela Petrova <mpetrovaa96@gmail.com>
2026-05-19 12:05:37 +0300

    done

--- fundamentals/ data_types_and_variables/centuries_to_minutes.py
+++ fundamentals/ data_types_and_variables/centuries_to_minutes.py
@@ -2,7 +2,7 @@ centuries = int(input())
 
 days_in_year = 365.2422
 years = centuries * 100
-days = years * days_in_year
+days = int(years * days_in_year)
 hours = days * 24
 minutes = hours * 60
 print(f'{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes')
--- fundamentals/basic_syntax,_conditional_statements_and_loops_lab/more_exercise/wolf_in_sheep's_clothing.py
+++ fundamentals/basic_syntax,_conditional_statements_and_loops_lab/more_exercise/wolf_in_sheep's_clothing.py
@@ -2,9 +2,10 @@ animals = input().split(", ")
 
 position = animals.index("wolf")
 
-if position == 0:
+if position == len(animals) - 1:
+    print("Please go away and stop eating my sheep")
+    sheep_number = len(animals) - 1
+
+else:
     sheep_number = len(animals) - position - 1
     print(f"Oi! Sheep number {sheep_number}! You are about to be eaten by a wolf!")
-else:
-    sheep_number = len(animals) - position
-    print("Please go away and stop eating my sheep")
